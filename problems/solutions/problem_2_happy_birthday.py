b51992c7b8784a227de58d3c6770c0ddfbf4e056
ariannedee <ariannedee@gmail.com>
2024-10-23 22:22:22 -0700

    Add problem 2

---
+++ problems/solutions/problem_2_happy_birthday.py
@@ -0,0 +1,9 @@
+"""
+Ask for the user's name and then sing happy birthday to them.
+"""
+name = input("What's your name? ").title()
+
+print("Happy birthday to you")
+print("Happy birthday to you")
+print("Happy birthday dear, " + name)
+print("Happy birthday to you")
