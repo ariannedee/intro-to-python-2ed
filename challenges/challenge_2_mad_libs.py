974236ab3f21f2733483f1c622f192512f50d76e
ariannedee <ariannedee@gmail.com>
2024-10-29 13:09:09 -0700

    Add challenge 2

---
+++ challenges/challenge_2_mad_libs.py
@@ -0,0 +1,15 @@
+"""
+Mad Libs
+"""
+
+# Todo: prompt the user to provide the missing words
+
+text = """
+Roses are {colour},
+Violets are blue,
+Sugar is {adjective},
+And so are you!
+"""
+
+# Todo: print the final text
+print(text)
---
+++ challenges/solutions/challenge_2_mad_libs.py
@@ -0,0 +1,12 @@
+"""
+Mad Libs
+"""
+colour = input('Colour: ').strip().lower()
+adjective = input('Adjective: ').strip().lower()
+
+print(f"""
+Roses are {colour},
+Violets are blue,
+Sugar is {adjective},
+And so are you!
+""")
