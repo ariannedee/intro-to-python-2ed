a62ebfc7d30a57adbb79ba17b63f0b6a8007e93d
ariannedee <ariannedee@gmail.com>
2024-10-29 12:00:44 -0700

    Update problem 1

---
+++ problems/problem_1_area_calculator.py
@@ -0,0 +1,10 @@
+"""
+Calculate the area of a right-angle triangle
+"""
+a = 3  # side 1
+b = 4  # side 2
+
+# Todo: calculate the area
+area = None
+
+print(area)
---
+++ problems/solutions/problem_1_area_calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculate the area of a right-angle triangle
+"""
+a = 3  # side 1
+b = 4  # side 2
+
+area = a * b / 2
+
+print(area)
