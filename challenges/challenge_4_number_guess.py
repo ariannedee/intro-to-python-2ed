ffe4493d1e2fd55cc85b275eba8719f1c1fa2cc8
ariannedee <ariannedee@gmail.com>
2024-10-29 21:49:10 -0700

    Add challenge 4

---
+++ challenges/challenge_4_number_guess.py
@@ -0,0 +1,12 @@
+"""
+Number guessing game
+
+You have 4 tries to guess a number between 1 and 20.
+"""
+import random
+
+
+answer = random.randint(1, 20)
+print("I'm thinking of a number between 1 and 20")
+
+guess = int(input("Make a guess: "))
