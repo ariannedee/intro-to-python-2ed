85f37c0afd8add000d9383a6c2ab7b1898745d44
ariannedee <ariannedee@gmail.com>
2024-11-07 16:30:08 -0800

    Update type hint challenge

--- challenges/solutions/challenge_5g_quiz_type_hints.py
+++ challenges/solutions/challenge_5g_quiz_type_hints.py
@@ -1,4 +1,12 @@
-answers = [
+"""
+Quiz game
+
+User will try to get all the answers (e.g. countries that start with V)
+If they give up and quit, print the ones that were missed
+"""
+
+v_country_category: str = 'Country that starts with V'
+v_countries: list[str] = [
     'Vanuatu',
     'Vatican City',
     'Venezuela',
@@ -6,38 +14,39 @@ answers = [
 ]
 
 
-def make_guess(prompt):
-    return input(prompt + ' (q to quit): ').strip()
-
-def game_over(guessed):
-    return len(answers) == len(guessed)
-
-def missed_answers(guessed):
-    missed = []
-    for answer in answers:
-        if answer not in guessed:
-            missed.append(answer)
-    return missed
-
-def answers_left(guessed):
-    return len(answers) - len(guessed)
-
-def play_game():
-    correct = []
-    while not game_over(correct):
-        print(f"{answers_left(correct)} left")
-        guess = make_guess('Countries that start with V')
-        if guess.lower() == 'q':
-            print('\nYou missed: ' + ', '.join(missed_answers(correct)))
-            return
-        elif guess in correct:
-            print('  Already guessed')
-        elif guess in answers:
-            print('  Correct!')
-            correct.append(guess)
-        else:
-            print('  Try again')
-    print('Great job!')
+def check_guess(guess: str, guessed: list[str], answers_left: list[str]) -> str:
+    if guess in guessed:
+        return 'Already guessed'
+    elif guess in answers_left:
+        guessed.append(guess)
+        answers_left.remove(guess)
+        return 'Correct!'
+    else:
+        return 'Try again'
 
 
-play_game()
+def get_result(answers_left: list[str]) -> str:
+    if len(answers_left) == 0:
+        return 'Great job!'
+    else:
+        missed = ', '.join(answers_left)
+        return 'You missed: ' + missed
+
+
+def play_game(category: str, answers: list[str]) -> None:
+    guessed = []
+    answers_left = answers.copy()
+
+    while len(answers_left) > 0:
+        print(f"{len(answers_left)} left")
+
+        guess = input(f"Enter a {category.lower()} (q to quit): ")
+        if guess == 'q':
+            break
+        print(check_guess(guess, guessed, answers_left))
+
+    print(get_result(answers_left))
+
+
+if __name__ == '__main__':
+    play_game(v_country_category, v_countries)
