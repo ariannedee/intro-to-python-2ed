74e2a2c1959a8c347755745f82e9303144a2b29a
ariannedee <ariannedee@gmail.com>
2025-01-06 12:19:40 -0800

    Update examples based on actual video

--- examples/example_12_lists.py
+++ examples/example_12_lists.py
@@ -1,44 +1,47 @@
-# Indexing and slicing
+# Creating lists
 letters = ['A', 'B', 'C', 'D']
+lowercase = list('abcd')  # ['a', 'b', 'c', 'd']
+empty = []
+also_empty = list()
 
+# Indexing and slicing
 print(letters[0])     # 'A'
 print(letters[-1])    # 'D'
 print(letters[1:3])   # ['B', 'C']
 print(letters[:2])    # ['A', 'B']
 print(letters[2:])    # ['C', 'D']
 print(letters[::-1])  # ['D', 'C', 'B', 'A']
-print()
 
-# ------------------------------
+# Length and containment
+print(len(letters))    # Get the length of a list
+print('A' in letters)  # Check if an item is in a list
 
 # Adding, removing, and updating
-languages = ['English', 'French', 'Tagalog', 'Mandarin']
+letters = ['A', 'B', 'C', 'D']
 
-languages[1] = 'Français'
-languages.append('Icelandic')    # Adds item to end of list
-languages.insert(1, 'Urdu')      # Adds item to list at index
-languages.remove('English')      # Remove first instance of item, raise error if not found
-print(languages)
-
-# ------------------------------
+letters[0] = 'a'                 # Update an item by index
+del letters[1]                   # Delete an item by index
+letters.append('E')              # Add an item to end of list
+letters.insert(1, 'B')           # Adds item to an index
+letters.extend(['A', 'B', 'C'])  # Adds each item from a sequence to the end of the list
+letters.remove('B')              # Remove first instance of item, raise error if not found
+print(letters)
 
 # More list methods
-letters.insert(1, True)    # Lists can hold any type of data
-letters.extend([1, 2, 3])  # Add contents of another sequence to the end
-print(letters)
-print(letters.count(1))  # Remember, 1 == True
+print(letters.count('C'))  # Returns 2, lists can have duplicates
+print(letters.pop())       # Removes and returns the last item
+print(letters.index('A'))  # Return the index of the item if found, raise ValueError if not found
 
-letters.reverse()  # Some methods return None, but alter the list
-print(letters)
-
-letters.sort()
+letters.reverse()  # Returns None, but reverses the original list
 print(letters)
 
+letters.sort()     # Returns None, but sorts the original list, raises TypeError if it can't be sorted
+print(letters)
 
 # Nested lists can be used for matrices
 matrix = [[1, 2, 3],
           [4, 5, 6],
           [7, 8, 9]]
 
-matrix[1][0] = 1000
+matrix[1][0] = 1000  # Change 4 to 1000
 print(matrix)
