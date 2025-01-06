6e89e706cad5bd43e227ae37edc196eb4b950fd3
ariannedee <ariannedee@gmail.com>
2025-01-06 10:53:58 -0800

    Update so it's all in one big function

--- challenges/solutions/challenge_4_number_guess.py
+++ challenges/solutions/challenge_4_number_guess.py
@@ -6,38 +6,42 @@ You have 3 tries to guess a number between 1 and 20.
 import random
 
 
-answer = random.randint(1, 20)
+def play_game():
+    answer = random.randint(1, 20)
 
-print("I'm thinking of a number between 1 and 20")
+    print("I'm thinking of a number between 1 and 20")
 
-guess = int(input("Make a guess: "))
+    guess = int(input("Make a guess: "))
 
-if answer == guess:
-    print('Got it!')
-    exit()
-elif answer > guess:
-    print('Higher')
-elif answer < guess:
-    print('Lower')
+    if answer == guess:
+        print('Got it!')
+        exit()
+    elif answer > guess:
+        print('Higher')
+    elif answer < guess:
+        print('Lower')
 
-guess = int(input("Make a guess: "))
+    guess = int(input("Make a guess: "))
 
-if answer == guess:
-    print('Got it!')
-    exit()
-elif answer > guess:
-    print('Higher')
-elif answer < guess:
-    print('Lower')
+    if answer == guess:
+        print('Got it!')
+        exit()
+    elif answer > guess:
+        print('Higher')
+    elif answer < guess:
+        print('Lower')
 
-guess = int(input("Make a guess: "))
+    guess = int(input("Make a guess: "))
 
-if answer == guess:
-    print('Got it!')
-    exit()
-elif answer > guess:
-    print('Higher')
-elif answer < guess:
-    print('Lower')
+    if answer == guess:
+        print('Got it!')
+        exit()
+    elif answer > guess:
+        print('Higher')
+    elif answer < guess:
+        print('Lower')
 
-print(f"It was {answer}")
+    print(f"It was {answer}")
+
+
+play_game()
